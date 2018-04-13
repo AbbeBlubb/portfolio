c1da6882719b0dca80bff429997962480cc4d106
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2018-04-14 01:50:16 +0200

    Component ProjectDescription

--- src/components/Project-description.js
+++ src/components/Project-description.js
@@ -1,10 +1,11 @@
 import React from "react"
 import data from "../data"
+import "./project-description.css"
 
 class ProjectDescription extends React.Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = { project: null }
   }
 
@@ -15,7 +16,7 @@ class ProjectDescription extends React.Component {
   fetchProject(){
     const project = data.find((project) => (
       project.id === this.props.match.params.projectId
-    ))
+    ));
 
     this.setState({
       project
@@ -31,9 +32,22 @@ class ProjectDescription extends React.Component {
   render() {
     if (this.state.project) {
       return (
-        <div>
-          <h2>{this.state.project.name}</h2>
-          <p>{this.state.project.description}</p>
+        <div className="project-description-main-container">
+          <div className="project-description-main-container-2">
+            <div className="project-description-container-for-img">
+              <div className="project-description-img-wrap">
+                <img className="project-description-img" src={require("../img/" + this.state.project.imgFileName)} />
+              </div>
+            </div>
+            <div className="project-description-container-for-text">
+              <div className="project-description-header-wrap">
+                <div className="project-description-header">{this.state.project.name}</div>
+              </div>
+              <div className="project-description-description-wrap">
+                <div className="project-description-description">{this.state.project.description}</div>
+              </div>
+            </div>
+          </div>
         </div>
       )
     } else {
--- src/components/Project-list.js
+++ src/components/Project-list.js
@@ -9,12 +9,12 @@ class ProjectList extends React.Component {
   render() {
     return (
       <div className="project-list-main-container">
-        <div className="project-list-container-for-background"></div>
+        <div className="project-list-empty-container-for-background"></div>
         {data.map((element) => (
           <div key={element.id}>
             <div className="project-list-container-for-container">
             <Link to={`/${element.id}`}>
-              <div className="project-list-container">
+              <div className="project-list-container-for-name-and-short">
                 <div className="project-list-name">{element.name}&nbsp;</div>
                 <div className="project-list-short">/ {element.short}</div>
               </div>
@@ -22,7 +22,7 @@ class ProjectList extends React.Component {
             </div>
           </div>
         ))}
-        <div className="project-list-container-for-background"></div>
+        <div className="project-list-empty-container-for-background"></div>
       </div>
     )
   }
