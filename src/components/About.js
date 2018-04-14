607028e039783cf53b4d6261de113ba1951c9877
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-04-14 14:46:36 +0200

    Component About

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,19 @@ class About extends React.Component {
   render() {
     return (
       <section className="about-main-container">
-        About me
-        <div className="about-text-about">About me</div>
-        <div className="about-text-contact">Kontakt</div>
+        <div className="about-container-for-about">
+          <div className="about-about-header">About me</div>
+          <div className="about-about-text">Once up on a time...</div>
+        </div>
+        <div className="about-container-for-contact">
+          <div className="about-contact-header">Contact</div>
+          <div className="about-contact-text">
+            <ul>
+              <li>My@email.yes</li>
+              <li>Maby LinkedIn</li>
+            </ul>
+          </div>
+        </div>
       </section>
     )
   }
