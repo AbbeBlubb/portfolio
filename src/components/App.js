556b46fb92ccb5b15fe0679e6b3d448f1e5e3160
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-04-16 20:51:25 +0200

    CSS clean-up

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="app-main-container">
+        <div className="app-main">
           <Header />
           <div>
             <Route path="/" component={ProjectList} />
