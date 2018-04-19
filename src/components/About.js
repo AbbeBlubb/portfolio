295a1694053de31a595fe8862b7733642b23311c
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-04-20 00:08:49 +0200

    Style for About

--- src/components/About.js
+++ src/components/About.js
@@ -6,18 +6,29 @@ class About extends React.Component {
   render() {
     return (
       <section className="about-main">
-        <div className="about">
-          <div className="about-header">About me</div>
-          <div className="about-text">Once up on a time...</div>
-        </div>
-        <div className="contact">
-          <div className="contact-header">Contact</div>
-          <div className="contact-text">
-            <ul>
-              <li>My@email.yes</li>
-              <li>Maby LinkedIn</li>
-            </ul>
+        <div className="container">
+          <div className="about">
+            <div className="about-header">
+              <h2>About me</h2></div>
+            <div className="about-text">
+              <p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. </p>
+              <p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. </p>
+            </div>
           </div>
+          <div className="contact">
+            <div className="contact-header">
+              <h2>Contact</h2>
+            </div>
+            <div className="contact-text">
+              <ul>
+                <li><span>My@email.yes</span></li>
+                <li>Maby LinkedIn</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="photo">
+          <div>Photo</div>
         </div>
       </section>
     )
