3cd07bb9b934cf4500ac67d048b370e5a67b384f
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-04-23 00:59:22 +0200

    CSS for ListItem and ProjectDescription

--- src/components/ProjectDescription.js
+++ src/components/ProjectDescription.js
@@ -6,18 +6,18 @@ class ProjectDescription extends React.Component {
   render() {
       return (
         <div className="pd-main">
-          <div className="pd-project">
-            <div className="container-img">
-              <div className="pd-img-wrap">
-                <img className="pd-img" src={require("../img/" + this.props.img)} />
+          <div className="project">
+            <div>
+              <div className="img-wrap">
+                <img className="img" src={require("../img/" + this.props.img)} />
               </div>
             </div>
             <div className="container-text">
-              <div className="pd-header-wrap">
-                <div className="pd-header">{this.props.name}</div>
+              <div className="header-wrap">
+                <div className="header">{this.props.name}</div>
               </div>
-              <div className="pd-description-wrap">
-                <div className="pd-description">{this.props.description}</div>
+              <div>
+                <div>{this.props.description}</div>
               </div>
             </div>
           </div>
