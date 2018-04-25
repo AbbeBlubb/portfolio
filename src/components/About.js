aaf4149e43294f351287a22e78051db46a7f0079
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-04-25 17:18:23 +0200

    About text + tryingn to position div

--- src/components/About.js
+++ src/components/About.js
@@ -5,32 +5,38 @@ class About extends React.Component {
 
   render() {
     return (
-      <section className="about-main">
-        <div className="container">
-          <div className="about">
-            <div className="about-header">
-              <h2>About me</h2></div>
-            <div className="about-text">
-              <p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. </p>
-              <p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. </p>
+      <footer className="about-main">
+          <div className="container">
+            <div className="about">
+              <div className="about-header">
+                <h2>About me</h2></div>
+              <div className="about-text">
+                <p>I'm a junior web developer. After eight years working with law I discovered frontend. At first I had the learning as a hobby, and soon I decided to go all-in. I love my new career path! </p>
+                <p>My experience with relevance for web developement is: 12 week intensive bootcamp in web developement / 5-week full-time course in C# / webcourse in Android Developement / evening course in responsive web design / own studies. </p>
+              </div>
+            </div>
+            <div className="contact">
+              <div className="contact-header">
+                <h2>Contact</h2>
+              </div>
+              <div className="contact-text">
+                <ul>
+                  <li><span>My@email.yes</span></li>
+                  <li>Maby LinkedIn</li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div className="contact">
-            <div className="contact-header">
-              <h2>Contact</h2>
-            </div>
-            <div className="contact-text">
-              <ul>
-                <li><span>My@email.yes</span></li>
-                <li>Maby LinkedIn</li>
-              </ul>
-            </div>
+
+          <div className="box1">
+            <div>Box1</div>
           </div>
+
+        <div className="box2">
+          <div>Box2</div>
         </div>
-        <div className="photo">
-          <div>Photo</div>
-        </div>
-      </section>
+
+      </footer>
     )
   }
 
