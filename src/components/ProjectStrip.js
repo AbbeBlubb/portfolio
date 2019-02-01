baf45c7696a3ab86324c685d9a1c7be33d5a361b
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2019-02-01 23:01:16 +0100

    Project area styling

--- src/components/ProjectStrip.js
+++ src/components/ProjectStrip.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export function ProjectStrip(props) {
-  return <div className="project-strip"><span className="project-strip__name">{props.name}</span>/ {props.short}</div>
+  return(
+    <div className="project-strip">
+      <div className="project-strip__text-wrap">
+        <span className="project-strip__name">
+          &nbsp;{props.name}
+        </span>
+        /&nbsp;{props.short}&nbsp;
+      </div>
+    </div>
+  )
 }
