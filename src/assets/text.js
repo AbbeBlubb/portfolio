76596af9818949dd7826810ca90a60c5c96e349a
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-02-03 16:04:03 +0100

    Canges in the project data

--- src/assets/text.js
+++ src/assets/text.js
@@ -3,37 +3,32 @@ import profilephoto from "./profilephoto.jpg"
 export default {
 
   hero: {
-    a: "Alberto",
-    fcp: "Francisco Chávez Pérez",
-    button: "Portfolio & me"
+    a: `Alberto`,
+    fcp: `Francisco Chávez Pérez`,
+    button: `Portfolio & me`
   },
 
   about: {
-    header: "About me",
+    header: `About me`,
     parraf1:
       "I'm a junior web developer. " +
-      "I have experience with HTML, CSS, SASS, LESS, JavaScript, React, JSX, Redux, Aurelia, Node.js, Express, MongoDB, Mongoose, Contentful, and more.",
+      `I have experience with agile methodology, small and big projects,
+      HTML, CSS, SASS, LESS,
+      JavaScript, React, Redux, JSX, Gatsby, Aurelia,
+      Node.js, Express, MongoDB, Mongoose,
+      Episerver, Contentful, GraphQL, Github, C#, and more.`,
     parraf2:
-      "After 8 years working with law I discovered web developing. " +
-      "At first I had the development as a hobby, and soon I decided to go all-in. " +
-      "I love my new career path! " +
-      "Law is a lot about structure, analysis and presentation. " +
-      "This experience I have brought into my web developer career!",
-    parraf3:
-      "My experience in development is: " +
-      "12 week intensive bootcamp in web development / " +
-      "5-week full-time course in C# / " +
-      "webcourse in Android Development / " +
-      "evening course in responsive web design / " +
-      "own studies.",
-    parraf4:
-      "This site is published May 14, 2018. I'll be deploying projects during May-June."
+      `After 8 years working with law I discovered web developing.
+      At first I had the development as a hobby, and soon I decided to go all-in.
+      I love my new career path!
+      Law is a lot about structure, analysis and presentation.
+      This experience I have brought into my web developer career!`,
   },
 
   contact: {
-    header: "Contact",
-    email: "alberto@francisco.nu",
-    linkedin: "https://www.linkedin.com/in/albertochavezperez/",
+    header: `Contact`,
+    email: `alberto@francisco.nu`,
+    linkedin: `https://www.linkedin.com/in/albertochavezperez/`,
     photo: profilephoto
   }
 
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,20 +1,20 @@
 import React from "react"
-import data from "../assets/data"
+import projectData from "../assets/project-data"
 import ProjectListItem from "./ProjectListItem"
 import { backgroundColor, backgroundImages } from '../assets/background-images'
 
 export function ProjectList() {
   return (
     <div className="project-list">
-      {data.map(element => (
+      {projectData.map(project => (
         <ProjectListItem
-          key={element.id}
-          id={element.id}
-          name={element.name}
-          short={element.short}
-          description={element.description}
-          link={element.link}
-          img={element.imgFileName}
+          key={project.id}
+          id={project.id}
+          name={project.name}
+          short={project.short}
+          description={project.description}
+          link={project.link}
+          img={project.imgFileName}
           backgroundColor={backgroundColor}
           backgroundImage={backgroundImages[Math.floor(Math.random()*backgroundImages.length)]}/>
       ))}
