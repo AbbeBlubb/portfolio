2c8b5f02515be7d2e75f6749fad8101c2d6d3b3e
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-02-10 17:55:10 +0100

    Bug fix - ProjectStrup text didn't resize fully when resizing from smaller to bigger screen
    Solution to put width: 100% on this div (despite the div is a block element)

--- src/components/ProjectStrip.js
+++ src/components/ProjectStrip.js
@@ -3,12 +3,11 @@ import React from "react";
 export function ProjectStrip(props) {
   return(
     <div className="project-strip">
-      <div 
+      <div
         className="project-strip__text-wrap"
-        style={{ backgroundColor: props.projectIsVisible && '#fbe5c6'}}>
-        <span className="project-strip__name">
-          {props.name}
-        </span>
+        style={{ backgroundColor: props.projectIsVisible && '#fbe5c6'}}
+      >
+        <span className="project-strip__name">{props.name}</span>
         /&nbsp;{props.short}
       </div>
     </div>
