0a4e098a5fcf9c09ac304fae89b617f23d951528
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-02-11 20:30:22 +0100

    Project name now has the inline styling bg-color when toggeling in the same element where the "box-decoration-break: clone" is

--- src/components/ProjectStrip.js
+++ src/components/ProjectStrip.js
@@ -3,11 +3,10 @@ import React from "react";
 export function ProjectStrip(props) {
   return(
     <div className="project-strip">
-      <div
-        className="project-strip__text-wrap"
-        style={{ backgroundColor: props.projectIsVisible && '#fbe5c6'}}
-      >
-        <span className='project-strip__text-line'>
+      <div className="project-strip__text-wrap">
+        <span
+            className='project-strip__text-line'
+            style={{ backgroundColor: props.projectIsVisible && '#fbe5c6'}}>
           <span className="project-strip__name">{props.name}</span>
           <span>/&nbsp;{props.short}</span>
         </span>
