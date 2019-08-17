d769f0c25e9729c6ca7b7c3a0ae38ebdfc103c62
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-08-17 19:48:07 +0200

    LanguageSelector hides on scroll down and shows on scroll up

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -4,9 +4,39 @@ import LanguageContext from '../contexts/LanguageContext';
 export class LanguageSelector extends React.Component {
   static contextType = LanguageContext;
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      prevScrollPos: window.pageYOffset,
+      languageSelectorIsHidden: false
+    };
+  }
+
+  handleScroll = () => {
+    const { prevScrollPos } = this.state;
+  
+    const currentScrollPos = window.pageYOffset;
+    const languageSelectorIsHidden = prevScrollPos < currentScrollPos; 
+    // current increases when scroll down, and decreases when scroll up. If previous Y is less than current Y = true. If previous Y is greater then current Y = false.
+  
+    this.setState({
+      prevScrollPos: currentScrollPos,
+      languageSelectorIsHidden
+    });
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
   render() {
     return (
-      <ul className="language-selector">
+      <ul className={`language-selector ${this.state.languageSelectorIsHidden && 'language-selector--hidden'}`}>
 
         <li className="language-selector__list-item">
           <button 
