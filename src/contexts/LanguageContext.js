9883e320f60c3b18872f0e47e63d683db94908af
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-08-18 22:49:06 +0200

    Implementation of cookie for language

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { docCookies } from "../assets/docCookies"
 
-const Context = React.createContext('en');
+const Context = React.createContext();
 
 export class LanguageStore extends React.Component {
-  state={ language: 'en' };
+  state={ 
+    language: docCookies.getItem('lang') || 'en' 
+  };
 
-handleLanguageChange = language => {
-  this.setState({ language });
-}
+  handleLanguageChange = language => {
+    if (language !== this.state.language) {
+      this.setState({ language });
+
+      docCookies.setItem('lang', language, this._maxAgeToGMT(Infinity));
+    }
+  }
+
+  // Convert relative time to absolute time in order to be compatible with IE, se docCookies.js
+  _maxAgeToGMT = nMaxAge => {
+    return nMaxAge === Infinity ? "Fri, 31 Dec 9999 23:59:59 GMT" : (new Date(nMaxAge * 1e3 + Date.now())).toUTCString();
+  }
 
   render() {
     return(
