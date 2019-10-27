7f1b0e3ba8a30d8f7bd6c9ec72238dc3996d2d52
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-10-27 12:16:01 +0100

    Refactoring of projectText.js

--- src/assets/texts/projectText.js
+++ src/assets/texts/projectText.js
@@ -1,194 +1,101 @@
+const fastEN = {
+  id: 'fast',
+  name: 'Fast loading',
+  short: 'Lazy-loading and code splitting',
+  description: `Images are lazy-loaded when entering the viewport.
+                Application code is splitted to facilitate caching.
+                This gives the user a fast-loading experience.
+                Bulit with plain JavaScript without framework.
+                Webpack configuration for code splitting, SCSS, dev server, Babel.
+                Responsive, mobile first.`,
+  imgFileName: 'fast.png',
+  linkUrl: 'https://abbeblubb.github.io/lazy-loading/',
+  linkText: 'Experience fast loading'
+};
+
+const scraperEN = {
+  id: 'scraper-node',
+  name: 'Fullstack Scraper',
+  short: 'News scraper with Node.js',
+  description: `The server scrapes the Aftonbladet news site and saves the headers with the corresponding links to a database.
+                The frontend displays these headers in a list, making it easy and fast to follow the general news.
+                JavaScript, Node, Express, Mongoose, React.
+                Responsive, mobile first.`,
+  imgFileName: 'scraper-node.png',
+  linkUrl: 'https://abbeblubb.github.io/scraper-node/',
+  linkText: 'Scrape the news site'
+};
+
+const taskmanagerEN = {
+  id: 'taskmanager',
+  name: 'Fullstack Task Manager',
+  short: 'Global State with Redux',
+  description: `Follow the Redux Store and Redux Actions in the console or in Redux DevTools.
+                API server hosted in Heroku, MongoDB server hosted in Atlas.
+                JavaScript, React, Redux, Redux-Thunk, Express, Mongoose, SASS.
+                Responsive, mobile first.`,
+  imgFileName: 'taskmanager.png',
+  linkUrl: 'https://abbeblubb.github.io/taskmanager/',
+  linkText: 'Look into the task manager'
+};
+
+const snakeEN = {
+  id: 'snake',
+  name: 'JavaScript Snake Game',
+  short: 'Eat apples and get bigger!',
+  description: `Make swipe gestures on the screen or use the keyboard arrows to control the snake.
+                Made with JavaScript.
+                Mobile first.`,
+  imgFileName: 'snake.png',
+  linkUrl: 'https://abbeblubb.github.io/snake/',
+  linkText: 'Play'
+};
+
+const stocksEN = {
+  id: 'stocks',
+  name: 'Fullstack Stocks Explorer',
+  short: 'Financial rating with global newsfeed',
+  description: `Explore the Swedish Large Cap list!
+                Explained financial information and up-to-date global newsfeed.
+                API server hosted in Heroku, MongoDB server hosted in mLab.
+                JavaScript, React, Express, Mongoose.
+                Responsive, mobile first.`,
+  imgFileName: 'value.png',
+  linkUrl: 'https://abbeblubb.github.io/stocks/',
+  linkText: 'See the project'
+};
+
 export const projectText = {
 
   en: [
-    {
-      id: 'fast',
-      name: 'Fast loading',
-      short: 'Lazy-loading and code splitting',
-      description: 'The images are lazy-loaded when entering the viewport. The code is splitted to facilitate caching. This gives the user a fast-loading experience. Bult with plain JavaScript without framework. The markup is built with template literals. Webpack configuration for code splitting, SCSS, dev server, Babel.',
-      imgFileName: 'fast.png',
-      linkUrl: 'https://abbeblubb.github.io/lazy-loading/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'scraper-node',
-      name: 'Fullstack Scraper',
-      short: 'News scraper with Node.js',
-      description: 'The server scrapes the Aftonbladet news site and saves the headers with the corresponding links to a database. The frontend displays these headers in a list, making it easy and fast to follow the general news. JavaScript, Node, Express, Mongoose, React. Responsive, mobile first.',
-      imgFileName: 'scraper-node.png',
-      linkUrl: 'https://abbeblubb.github.io/scraper-node/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'taskmanager',
-      name: 'Fullstack Task Manager',
-      short: 'Global State with Redux',
-      description: 'Follow the Redux Store and Redux Actions in the console or in Redux DevTools. Search bar to easily find persons. API server hosted in Heroku, MongoDB server hosted in Atlas. JavaScript, React, Redux, Redux-Thunk, Express, Mongoose, SASS. Responsive, mobile first.',
-      imgFileName: 'taskmanager.png',
-      linkUrl: 'https://abbeblubb.github.io/taskmanager/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'snake',
-      name: 'JavaScript Snake Game',
-      short: 'Eat apples and get bigger!',
-      description: 'Make swipe gestures on the screen or use the keyboard arrows to control the snake. Made with JavaScript. Mobile first.',
-      imgFileName: 'snake.png',
-      linkUrl: 'https://abbeblubb.github.io/snake/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'stocks',
-      name: 'Fullstack Stocks Explorer',
-      short: 'Financial rating with global newsfeed',
-      description: 'Explore the Swedish Large Cap list! Explained financial information and up-to-date global newsfeed. API server hosted in Heroku, MongoDB server hosted in mLab. JavaScript, React, Express, Mongoose. Responsive, mobile first.',
-      imgFileName: 'value.png',
-      linkUrl: 'https://abbeblubb.github.io/stocks/',
-      linkText: 'See the project'
-    }
+    fastEN,
+    scraperEN,
+    taskmanagerEN,
+    snakeEN,
+    stocksEN
   ],
 
   es: [
-    {
-      id: 'fast',
-      name: 'Fast loading',
-      short: 'Lazy-loading and code splitting',
-      description: 'The images are lazy-loaded when entering the viewport. The code is splitted to facilitate caching. This gives the user a fast-loading experience. Bult with plain JavaScript without framework. The markup is built with template literals. Webpack configuration for code splitting, SCSS, dev server, Babel.',
-      imgFileName: 'fast.png',
-      linkUrl: 'https://abbeblubb.github.io/lazy-loading/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'scraper-node',
-      name: 'Fullstack Scraper',
-      short: 'News scraper with Node.js',
-      description: 'The server scrapes the Aftonbladet news site and saves the headers with the corresponding links to a database. The frontend displays these headers in a list, making it easy and fast to follow the general news. JavaScript, Node, Express, Mongoose, React. Responsive, mobile first.',
-      imgFileName: 'scraper-node.png',
-      linkUrl: 'https://abbeblubb.github.io/scraper-node/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'taskmanager',
-      name: 'Fullstack Task Manager',
-      short: 'Global State with Redux',
-      description: 'Follow the Redux Store and Redux Actions in the console or in Redux DevTools. Search bar to easily find persons. API server hosted in Heroku, MongoDB server hosted in Atlas. JavaScript, React, Redux, Redux-Thunk, Express, Mongoose, SASS. Responsive, mobile first.',
-      imgFileName: 'taskmanager.png',
-      linkUrl: 'https://abbeblubb.github.io/taskmanager/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'snake',
-      name: 'JavaScript Snake Game',
-      short: 'Eat apples and get bigger!',
-      description: 'Make swipe gestures on the screen or use the keyboard arrows to control the snake. Made with JavaScript. Mobile first.',
-      imgFileName: 'snake.png',
-      linkUrl: 'https://abbeblubb.github.io/snake/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'stocks',
-      name: 'Fullstack Stocks Explorer',
-      short: 'Financial rating with global newsfeed',
-      description: 'Explore the Swedish Large Cap list! Explained financial information and up-to-date global newsfeed. API server hosted in Heroku, MongoDB server hosted in mLab. JavaScript, React, Express, Mongoose. Responsive, mobile first.',
-      imgFileName: 'value.png',
-      linkUrl: 'https://abbeblubb.github.io/stocks/',
-      linkText: 'See the project'
-    },
+    fastEN,
+    scraperEN,
+    taskmanagerEN,
+    snakeEN,
+    stocksEN
   ],
 
   ru: [
-    {
-      id: 'fast',
-      name: 'Fast loading',
-      short: 'Lazy-loading and code splitting',
-      description: 'The images are lazy-loaded when entering the viewport. The code is splitted to facilitate caching. This gives the user a fast-loading experience. Bult with plain JavaScript without framework. The markup is built with template literals. Webpack configuration for code splitting, SCSS, dev server, Babel.',
-      imgFileName: 'fast.png',
-      linkUrl: 'https://abbeblubb.github.io/lazy-loading/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'scraper-node',
-      name: 'Fullstack Scraper',
-      short: 'News scraper with Node.js',
-      description: 'The server scrapes the Aftonbladet news site and saves the headers with the corresponding links to a database. The frontend displays these headers in a list, making it easy and fast to follow the general news. JavaScript, Node, Express, Mongoose, React. Responsive, mobile first.',
-      imgFileName: 'scraper-node.png',
-      linkUrl: 'https://abbeblubb.github.io/scraper-node/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'taskmanager',
-      name: 'Fullstack Task Manager',
-      short: 'Global State with Redux',
-      description: 'Follow the Redux Store and Redux Actions in the console or in Redux DevTools. Search bar to easily find persons. API server hosted in Heroku, MongoDB server hosted in Atlas. JavaScript, React, Redux, Redux-Thunk, Express, Mongoose, SASS. Responsive, mobile first.',
-      imgFileName: 'taskmanager.png',
-      linkUrl: 'https://abbeblubb.github.io/taskmanager/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'snake',
-      name: 'JavaScript Snake Game',
-      short: 'Eat apples and get bigger!',
-      description: 'Make swipe gestures on the screen or use the keyboard arrows to control the snake. Made with JavaScript. Mobile first.',
-      imgFileName: 'snake.png',
-      linkUrl: 'https://abbeblubb.github.io/snake/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'stocks',
-      name: 'Fullstack Stocks Explorer',
-      short: 'Financial rating with global newsfeed',
-      description: 'Explore the Swedish Large Cap list! Explained financial information and up-to-date global newsfeed. API server hosted in Heroku, MongoDB server hosted in mLab. JavaScript, React, Express, Mongoose. Responsive, mobile first.',
-      imgFileName: 'value.png',
-      linkUrl: 'https://abbeblubb.github.io/stocks/',
-      linkText: 'See the project'
-    }
+    fastEN,
+    scraperEN,
+    taskmanagerEN,
+    snakeEN,
+    stocksEN
   ],
 
   sv: [
-    {
-      id: 'fast',
-      name: 'Fast loading',
-      short: 'Lazy-loading and code splitting',
-      description: 'The images are lazy-loaded when entering the viewport. The code is splitted to facilitate caching. This gives the user a fast-loading experience. Bult with plain JavaScript without framework. The markup is built with template literals. Webpack configuration for code splitting, SCSS, dev server, Babel.',
-      imgFileName: 'fast.png',
-      linkUrl: 'https://abbeblubb.github.io/lazy-loading/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'scraper-node',
-      name: 'Fullstack Scraper',
-      short: 'News scraper with Node.js',
-      description: 'The server scrapes the Aftonbladet news site and saves the headers with the corresponding links to a database. The frontend displays these headers in a list, making it easy and fast to follow the general news. JavaScript, Node, Express, Mongoose, React. Responsive, mobile first.',
-      imgFileName: 'scraper-node.png',
-      linkUrl: 'https://abbeblubb.github.io/scraper-node/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'taskmanager',
-      name: 'Fullstack Task Manager',
-      short: 'Global State with Redux',
-      description: 'Follow the Redux Store and Redux Actions in the console or in Redux DevTools. Search bar to easily find persons. API server hosted in Heroku, MongoDB server hosted in Atlas. JavaScript, React, Redux, Redux-Thunk, Express, Mongoose, SASS. Responsive, mobile first.',
-      imgFileName: 'taskmanager.png',
-      linkUrl: 'https://abbeblubb.github.io/taskmanager/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'snake',
-      name: 'JavaScript Snake Game',
-      short: 'Eat apples and get bigger!',
-      description: 'Make swipe gestures on the screen or use the keyboard arrows to control the snake. Made with JavaScript. Mobile first.',
-      imgFileName: 'snake.png',
-      linkUrl: 'https://abbeblubb.github.io/snake/',
-      linkText: 'See the project'
-    },
-    {
-      id: 'stocks',
-      name: 'Fullstack Stocks Explorer',
-      short: 'Financial rating with global newsfeed',
-      description: 'Explore the Swedish Large Cap list! Explained financial information and up-to-date global newsfeed. API server hosted in Heroku, MongoDB server hosted in mLab. JavaScript, React, Express, Mongoose. Responsive, mobile first.',
-      imgFileName: 'value.png',
-      linkUrl: 'https://abbeblubb.github.io/stocks/',
-      linkText: 'See the project'
-    }
+    fastEN,
+    scraperEN,
+    taskmanagerEN,
+    snakeEN,
+    stocksEN
   ]
 };
