4f304114712cf125e7a6883d3a3f2e60ef0cb8c9
Alberto Chávez Pérez <alberto@francisco.nu>
2020-11-11 21:39:24 +0100

    Experiment with icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 import { headerText } from '../assets/texts/headerText';
 
+
 export class Header extends React.Component {
   static contextType = LanguageContext;
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.refForTitleContainer = React.createRef();
   }
@@ -15,8 +16,8 @@ export class Header extends React.Component {
 
     return (
       <header
-          className='header'
-          ref={this.refForTitleContainer}>
+        className='header'
+        ref={this.refForTitleContainer}>
 
         <h1 className='header__heading'>
           <span>{headerText[language].a}</span> {headerText[language].fcp}
@@ -24,13 +25,13 @@ export class Header extends React.Component {
 
         {/* Position the button-wrapper in absolute, so the button inside the wrapper don't need to have the absolute positioning */}
         <div className="header__button-wrapper">
-              {/* The button has the styling, and the wave-effect that requires relative positioning */}
-              <button
-                  className="header__button waves-effect waves-light"
-                  onClick={() => this.props.handleScrollToRef()}
-              >
-                {headerText[language].button}
-              </button>
+          {/* The button has the styling, and the wave-effect that requires relative positioning */}
+          <button
+            className="header__button waves-effect waves-light"
+            onClick={() => this.props.handleScrollToRef()}
+          >
+            {headerText[language].button}
+          </button>
         </div>
       </header>
     );
