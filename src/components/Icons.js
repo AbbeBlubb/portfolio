8e4701d69dbade248bbfce73608187d009b897fd
Alberto Chávez Pérez <andalusier@pm.me>
2020-11-14 03:07:01 +0100

    Icons component

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,127 +2,129 @@ import React from "react";
 
 export function Icons() {
   return (
-    <ul className="icon-list">
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
-          className="icon"
-          alt="JavaScript logo"
-          title="Javascript"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"
-          className="icon"
-          alt="TypeScript logo"
-          title="TypeScript"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"
-          className="icon"
-          alt="React logo"
-          title="React"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/redux/redux.png"
-          className="icon"
-          alt="Redux logo"
-          title="Redux"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/webpack/webpack.png"
-          className="icon"
-          alt="Webpack logo"
-          title="Webpack"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/docker/docker.png"
-          className="icon"
-          alt="Docker logo"
-          title="Docker"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"
-          className="icon"
-          alt="NodeJS logo"
-          title="NodeJS"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/express/express.png"
-          className="icon"
-          alt="Express logo"
-          title="Express"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/graphql/graphql.png"
-          className="icon"
-          alt="GraphQL logo"
-          title="GraphQL"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png"
-          className="icon"
-          alt="MongoDB logo"
-          title="MmongoDB"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongoose/mongoose.png"
-          className="icon"
-          alt="Mongoose logo"
-          title="Mongoose"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/ubuntu/ubuntu.png"
-          className="icon"
-          alt="Ubuntu logo"
-          title="Ubuntu"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/linux/linux.png"
-          className="icon"
-          alt="Linux logo"
-          title="Linux"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/bash/bash.png"
-          className="icon"
-          alt="Bash logo"
-          title="Bash"
-        />
-      </li>
-      <li className="icon-list-item">
-        <img
-          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/azure/azure.png"
-          className="icon"
-          alt="Azure logo"
-          title="Azure DevOps"
-        />
-      </li>
-    </ul>
+    <div className="icons">
+      <ul className="icons__list">
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
+            className="icons__icon"
+            alt="JavaScript logo"
+            title="Javascript"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"
+            className="icons__icon"
+            alt="TypeScript logo"
+            title="TypeScript"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"
+            className="icons__icon"
+            alt="React logo"
+            title="React"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/redux/redux.png"
+            className="icons__icon"
+            alt="Redux logo"
+            title="Redux"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/webpack/webpack.png"
+            className="icons__icon"
+            alt="Webpack logo"
+            title="Webpack"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/docker/docker.png"
+            className="icons__icon"
+            alt="Docker logo"
+            title="Docker"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"
+            className="icons__icon"
+            alt="NodeJS logo"
+            title="NodeJS"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/express/express.png"
+            className="icons__icon"
+            alt="Express logo"
+            title="Express"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/graphql/graphql.png"
+            className="icons__icon"
+            alt="GraphQL logo"
+            title="GraphQL"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png"
+            className="icons__icon"
+            alt="MongoDB logo"
+            title="MmongoDB"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongoose/mongoose.png"
+            className="icons__icon"
+            alt="Mongoose logo"
+            title="Mongoose"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/ubuntu/ubuntu.png"
+            className="icons__icon"
+            alt="Ubuntu logo"
+            title="Ubuntu"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/linux/linux.png"
+            className="icons__icon"
+            alt="Linux logo"
+            title="Linux"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/bash/bash.png"
+            className="icons__icon"
+            alt="Bash logo"
+            title="Bash"
+          />
+        </li>
+        <li className="icons__list-item">
+          <img
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/azure/azure.png"
+            className="icons__icon"
+            alt="Azure logo"
+            title="Azure DevOps"
+          />
+        </li>
+      </ul>
+    </div>
   );
 }
